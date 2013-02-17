88e39f0825870a7f5bcdeafe67e21579f116823d
rbsec <robin@rbsec.net>
2013-02-17 23:08:12 +0000

    Moved get_dir to own method

--- pybuster.py
+++ pybuster.py
@@ -4,20 +4,20 @@ import threading
 import urllib2
 import sys
 
+# Usage: pybuster.py <domain name>
+
 target = sys.argv[1]
 
 wordlist = ["admin", "test", "redmine", "robots.txt", "secret"]
 	            
 queue = Queue.Queue()
           
-class getUrl(threading.Thread):
+class queue_manager(threading.Thread):
     def __init__(self, queue):
         threading.Thread.__init__(self)
         self.queue = queue
 
-    def run(self):
-        while True:
-            word = self.queue.get(timeout=2)
+    def get_dir(self, word):
             request = urllib2.Request("http://" + target + "/" + word)
             request.get_method = lambda : 'HEAD'
 
@@ -29,11 +29,16 @@ class getUrl(threading.Thread):
             else:       # 200
                 print word + " - 200"
 
+    def run(self):
+        while True:
+            word = self.queue.get(timeout=2)
+            self.get_dir(word)
             self.queue.task_done()
+
 
 def main():
     for i in range(2):      #Number of threads
-        t = getUrl(queue)
+        t = queue_manager(queue)
         t.setDaemon(True)
         t.start()
 
